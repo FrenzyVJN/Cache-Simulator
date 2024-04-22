53126876f00ca1ecbfcc56e78d94e9d553eefa81
FrenzyVJN <svijayan01@gmail.com>
2024-04-22 17:44:43 +0530

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="md:text-3xl">Cache Simulator</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Vijayan
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href=""
+					target="_blank"
+					rel="noreferrer"
+				>
+					
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href=""
+					target="_blank"
+					rel="noreferrer"
+				>
+					
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<div class="flex flex-row h-full gap-5">
+<div class="h-full w-1/4">
+	<!-- Number of lines in Main Memory -->
+	{#each new Array(1024) as _, index}
+	<div class="border h-1/16 w- flex justify-center items-center">Main Memory</div>
+	{/each}
+</div>
+<div class="h-full w-1/4">
+	<!-- Number of lines in L2 -->
+	{#each new Array(256) as _, index}
+	<div class="border h-1/16 w-full">L2</div>
+	{/each}
+</div>
+<div class="h-full w-1/4">
+	<!-- Number of lines in L1 -->
+    {#each new Array(128) as _, index}
+      <div class="border h-1/16 w-full">L1</div>
+    {/each}
+	<br />
+	<div>
+		<!-- Number of lines in Victim Cache -->
+		{#each new Array(4) as _, index}
+			<div class="border h-1/2 w-full">Victim Cache</div>
+		{/each}
+	</div>
+</div>
+<div class="h-1/6 w-1/4">
+	<div class="h-1/6 border justify-center items-center flex">Processor</div>
+</div>
+</div>
