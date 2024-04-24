684b05e833a784f976558b8cdefbf4ffcd2f88ff
FrenzyVJN <svijayan01@gmail.com>
2024-04-24 13:25:10 +0530

    basic UI ready

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,63 @@
-<div class="flex flex-row h-full gap-5">
+<script>
+	/**
+	 * @type {string}
+	 */
+	let option;
+	console.log("hello")
+</script>
+<div class="flex flex-row h-full gap-5 p-5">
 <div class="h-full w-1/4">
 	<!-- Number of lines in Main Memory -->
 	{#each new Array(1024) as _, index}
-	<div class="border h-1/16 w- flex justify-center items-center">Main Memory</div>
+	<div class=" h-1/16 border rounded-lg w- flex justify-center items-center p-1">Main Memory</div>
 	{/each}
 </div>
 <div class="h-full w-1/4">
 	<!-- Number of lines in L2 -->
 	{#each new Array(256) as _, index}
-	<div class="border h-1/16 w-full">L2</div>
+	<div class="border h-1/16 w-full rounded-lg p-1">L2</div>
 	{/each}
 </div>
 <div class="h-full w-1/4">
 	<!-- Number of lines in L1 -->
     {#each new Array(128) as _, index}
-      <div class="border h-1/16 w-full">L1</div>
+      <div class="border h-1/16 w-full rounded-lg p-1">L1</div>
     {/each}
 	<br />
 	<div>
 		<!-- Number of lines in Victim Cache -->
 		{#each new Array(4) as _, index}
-			<div class="border h-1/2 w-full">Victim Cache</div>
+			<div class="border h-1/2 w-full rounded-lg p-1">Victim Cache</div>
 		{/each}
 	</div>
 </div>
-<div class="h-1/6 w-1/4">
-	<div class="h-1/6 border justify-center items-center flex">Processor</div>
+<div class="w-1/4">
+	<div class="h-96 border rounded-lg">
+		<div class="mt-16 flex justify-center">
+			<select bind:value={option} class="flex mr-5 text-white p-1 rounded-lg bg-blue-500 bg-opacity-30">
+				<option selected value="">Select Option</option>
+				<option value="load">Load</option>
+				<option value="store">Store</option>
+			</select>
+			<input type="text" class="bg-white bg-opacity-20 rounded-lg">
+		</div>
+		<div class="pt-10 flex justify-center">
+			{#if option == "load"}
+			<button class="border rounded-lg p-1 bg-blue-800 bg-opacity-20">Load</button>
+			{:else if option == "store"}
+			<button class="border rounded-lg p-1 bg-blue-800 bg-opacity-20">Store</button>
+			{:else if option == ""}
+			<button class="border rounded-lg p-1 bg-blue-800 bg-opacity-20" disabled>Choose option first</button>
+			{/if}
+			</div>
+		<div class="h-10 mt-10 px-2 flex">
+			<div class="border rounded-l-lg h-full w-1/3"></div>
+			<div class="border h-full w-1/3"></div>
+			<div class="border rounded-r-lg h-full w-1/3"></div>
+		</div>
+		<div class="px-2 mt-2">
+			<div class="border rounded-lg px-2 flex justify-center pb-20">Information</div>
+		</div>
+	</div>
 </div>
 </div>
